b540f8c1cb98bbec71566184a84fa64c8a1c5754
Ondřej Dudáček <114053208+OndrejDudacek@users.noreply.github.com>
2022-10-19 09:40:54 +0200

    Add files via upload

---
+++ 19.10. 22 #02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong num = ulong.Parse(Console.ReadLine());
+            bool prime = true;
+
+            if (num % 2 == 0)
+            {
+                prime = false;
+            }
+            else
+            {
+                for (ulong i = 2; i < num; i++)
+                {
+            
+                    if (num % i == 0)
+                    {
+                    prime = false;
+                    }
+                }
+            }
+
+            if (prime)
+            {
+
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
