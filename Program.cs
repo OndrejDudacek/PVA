941e5a7164c7548c80be0607334c9f619e2723cf
Ondřej Dudáček <114053208+OndrejDudacek@users.noreply.github.com>
2022-11-02 08:37:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._10._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int min = Int32.MaxValue;
+            int max = -1;
+            int cislo;
+            int soucet = 0;
+
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                Console.Write("Číslo: ");
+                Console.WriteLine(i);
+                cislo = Convert.ToInt32(Console.ReadLine());
+
+
+                soucet += cislo;
+
+                if (cislo > max)
+                {
+                    max = cislo;
+                }
+                if (cislo < min)
+                {
+                    min = cislo;
+                }
+
+            }
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Max:" + max);
+            Console.WriteLine("Min:" + min);
+            Console.WriteLine("Průměr:" + soucet / x);
+
+            Console.ReadKey();
+        }
+    }
+}
